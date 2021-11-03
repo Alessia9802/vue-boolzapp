51f419abf5c34ce9f0dfb499c02c194b91f8456c
Alessia9802 <alessiamisciagna@yahoo.it>
2021-11-03 16:05:35 +0100

    contact list doesn't work properly

--- Milestone_1/assets/js/script.js
+++ Milestone_1/assets/js/script.js
@@ -4,15 +4,19 @@ const app = new Vue({
     contatti: [
       {
         nome: "Michele",
+        img: "./assets/img/avatar_2.jpg",
       },
       {
         nome: "Fabio",
+        img: "./assets/img/avatar_3.jpg",
       },
       {
         nome: "Samuele",
+        img: "./assets/img/avatar_4.jpg",
       },
       {
         nome: "Luisa",
+        img: "./assets/img/avatar_5.jpg",
       },
     ],
   },
